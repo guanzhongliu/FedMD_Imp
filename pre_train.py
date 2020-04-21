1f3f31d1a1d0126994eb15753c4b21a7096564b3
Septieme <rogerliu00425@gmail.com>
2020-04-21 17:20:16 +0800

    Add FedMD_random

--- pre_train.py
+++ pre_train.py
@@ -27,7 +27,15 @@ def parseArg():
 models = {"2_layer_CNN": cnn_2layer_fc_model,
           "3_layer_CNN": cnn_3layer_fc_model}
 
-
+# EarlyStopping允许设置的终止训练的条件即参数如下：
+#
+# monitor：监控的数据接口。keras定义了如下的数据接口可以直接使用：
+# patient：对于设置的monitor，可以忍受在多少个epoch内没有改进？
+# min_delta：评判monitor是否有改进的标准，只有变动范围大于min_delta的monitor才算是改进。对于连续在patient个epoch内没有改进的情况执行EarlyStopping。
+# mode：只有三种情况{‘min’,’max’,’auto’}，分别表示monitor正常情况下是上升还是下降。比如当monitor为acc时mode要设置为’max’，因为正确率越大越好，相反，当monitor为loss时mode要设置为’min’。
+# verbose：是否输出更多的调试信息。
+# baseline：monitor的基线，即当monitor在基线以上没有改进时EarlyStopping。
+# restore_best_weights：当发生EarlyStopping时，模型的参数未必是最优的，即monitor的指标未必处于最优状态。如果restore_best_weights设置为True，则自动查找最优的monitor指标时的模型参数。
 def train_models(models, X_train, y_train, X_test, y_test,
                  is_show=False, save_dir="./", save_names=None,
                  early_stopping=True,
